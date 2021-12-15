61f2364f393c04133bbbb69d0850921b257ac771
Glenn Ren <glenn.y.ren@gmail.com>
2021-12-15 15:12:50 -0500

    Uncomment empty commit code

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ from client_write import GithubWriteClient
 
 reader = GithubReadClient()
 contributions = reader.calculate_contributions()
-# writer = GithubWriteClient()
-# writer.create_commits_for_contributions(contributions)
-print(contributions)
+writer = GithubWriteClient()
+writer.create_commits_for_contributions(contributions)
